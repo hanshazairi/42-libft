cf0aa47a10cf0579a458a999372794a33742486d
Hans Hazairi <hanshazairi@gmail.com>
2021-04-28 13:02:25 +0800

    Added & updated files
    - Added ft_isalnum.c
    - Updated libft.h

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,20 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/27 15:21:06 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/04/28 12:49:01 by hbaddrul         ###   ########.fr       */
+/*   Created: 2021/04/28 12:47:05 by hbaddrul          #+#    #+#             */
+/*   Updated: 2021/04/28 13:00:16 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int		ft_isalnum(int c);
 
-#endif
+int		ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
