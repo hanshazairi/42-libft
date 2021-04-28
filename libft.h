8dc0ce84d0f32f3b94c44bc6520e4fe3e277e6fb
Hans Hazairi <hanshazairi@gmail.com>
2021-04-28 13:22:32 +0800

    Added & updated files
    - Added ft_isascii.c
    - Updated libft.h
    - Corrected Norm error for ft_isdigit.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 15:21:06 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/04/28 12:49:01 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/04/28 13:18:17 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 #endif
