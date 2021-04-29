fa14a55054392cd22b9bd99c14b09795bade42ec
Hans Hazairi <hanshazairi@gmail.com>
2021-04-29 12:34:29 +0800

    Added & updated files
    - Added ft_strlen.c
    - Updated libft.h

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/27 15:21:06 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/04/29 12:30:42 by hbaddrul         ###   ########.fr       */
+/*   Created: 2021/04/29 12:26:09 by hbaddrul          #+#    #+#             */
+/*   Updated: 2021/04/29 12:31:57 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+size_t	ft_strlen(const char *s)
+{
+	size_t	ret;
 
-size_t	ft_strlen(const char *s);
-int		ft_atoi(const char *str);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-#endif
+	ret = 0;
+	while (*s++)
+		++ret;
+	return (ret);
+}
