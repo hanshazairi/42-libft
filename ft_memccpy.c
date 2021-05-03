930a900bc2ba49221034a4fb915d40f169884312
Hans Hazairi <hanshazairi@gmail.com>
2021-05-03 15:06:21 +0800

    Added & updated files
    - Added ft_memccpy.c
    - Updated libft.h

---
+++ ft_memccpy.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/03 14:12:59 by hbaddrul          #+#    #+#             */
+/*   Updated: 2021/05/03 15:02:57 by hbaddrul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	while (n--)
+	{
+		*(char *)dst++ = *(char *)src;
+		if (*(unsigned char *)src++ == (unsigned char)c)
+			return (dst);
+	}
+	return (0);
+}
