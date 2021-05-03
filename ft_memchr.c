9af58cc315bf0bb22bd15ffeb7067d2da62739a2
Hans Hazairi <hanshazairi@gmail.com>
2021-05-03 17:59:35 +0800

    Added & updated files
    - Added ft_memchr.c
    - Updated libft.h

---
+++ ft_memchr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/03 17:54:42 by hbaddrul          #+#    #+#             */
+/*   Updated: 2021/05/03 17:58:16 by hbaddrul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while (n--)
+	{
+		if (*(unsigned char *)s == (unsigned char)c)
+			return ((void *)s);
+		++s;
+	}
+	return (0);
+}
