1ddc42f35cb4903126f39a303be507306ecf6361
Hans Hazairi <hanshazairi@gmail.com>
2021-05-08 00:57:26 +0800

    Added & updated files
    - Added ft_putendl_fd.c
    - Updated libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 15:21:06 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/08 00:54:06 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/08 00:56:35 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
