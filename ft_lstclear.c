e3d453964f843691c17040b3498ef28d87441ffb
Hans Hazairi <hanshazairi@gmail.com>
2021-05-08 01:42:22 +0800

    Added & updated files
    - Added ft_listclear.c
    - Updated libft.h

---
+++ ft_lstclear.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/08 01:37:14 by hbaddrul          #+#    #+#             */
+/*   Updated: 2021/05/08 01:39:05 by hbaddrul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (lst && *lst)
+	{
+		ft_lstclear(&(*lst)->next, del);
+		ft_lstdelone(*lst, del);
+		*lst = 0;
+	}
+}
