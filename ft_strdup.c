17cfe33ada4ad5aad8aa75ba08b68b411904afdc
Hans Hazairi <hanshazairi@gmail.com>
2021-05-09 01:59:37 +0800

    Updated ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 19:06:40 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/05 21:23:12 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/09 01:58:34 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*ret;
-	size_t	i;
 	size_t	len;
 
 	len = ft_strlen(s1);
 	ret = malloc(len + 1);
 	if (!ret)
 		return (0);
-	i = 0;
-	while (len--)
-		ret[i++] = *s1++;
-	ret[i] = 0;
+	ft_strlcpy(ret, s1, len + 1);
 	return (ret);
 }
