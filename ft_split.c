5caa6fd830cb5a8156a9cab2cd604791fccb3cc5
Hans Hazairi <hanshazairi@gmail.com>
2021-05-09 04:27:42 +0800

    Updated ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 17:02:38 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/07 18:07:28 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/09 04:24:29 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	getwordcount(const char *s, char c)
+static size_t	scount(const char *s, char c)
 {
 	size_t	count;
 
@@ -31,27 +31,40 @@ static size_t	getwordcount(const char *s, char c)
 	return (count);
 }
 
-char	**ft_split(const char *s, char c)
+static char	*ft_strndup(const char *s1, size_t n)
 {
-	char	*start;
-	char	**ret;
-	size_t	i;
+	char	*ret;
+	size_t	len;
 
-	ret = malloc(sizeof(char *) * getwordcount(s, c) + 1);
+	if (n < ft_strlen(s1))
+		len = n;
+	else
+		len = ft_strlen(s1);
+	ret = malloc(len + 1);
 	if (!ret)
 		return (0);
+	ft_strlcpy(ret, s1, len + 1);
+	return (ret);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**ret;
+	size_t	i;
+	size_t	len;
+
 	i = 0;
+	ret = malloc(sizeof(char *) * scount(s, c) + 1);
+	if (!ret)
+		return (0);
 	while (*s)
 	{
 		if (*s != c)
 		{
-			start = (char *)s;
-			while (*s && *s != c)
+			len = 0;
+			while (*s && *s != c && ++len)
 				++s;
-			ret[i] = malloc(s - start + 1);
-			if (!ret[i])
-				return (0);
-			ft_strlcpy(ret[i++], start, s - start + 1);
+			ret[i++] = ft_strndup(s - len, len);
 		}
 		else
 			++s;
