99c50dfa91394a68dd34d9c44c9b68ead9e780eb
Hans Hazairi <hanshazairi@gmail.com>
2021-05-09 05:45:58 +0800

    Updated ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 15:35:45 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/07 15:42:19 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/09 05:44:44 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	char	*ret;
 	size_t	len;
 
+	if (!s1 || !s2)
+		return (0);
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	ret = malloc(len);
 	if (!ret)
