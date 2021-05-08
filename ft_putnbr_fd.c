f2219552c5f5c595339262902c76436dd96089fc
Hans Hazairi <hanshazairi@gmail.com>
2021-05-09 01:47:48 +0800

    Updated ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 00:57:40 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/08 01:04:17 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/09 01:46:58 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	ft_putnbr_fd(int n, int fd)
 	sign = 1;
 	if (n < 0)
 	{
-		write(fd, "-", 1);
+		ft_putchar_fd('-', fd);
 		sign = -1;
 	}
 	if (n / 10)
 		ft_putnbr_fd(n / 10 * sign, fd);
 	c = '0' + n % 10 * sign;
-	write(fd, &c, 1);
+	ft_putchar_fd(c, fd);
 }
