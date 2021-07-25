96632be1cfba3f9af083055b1b616db6f34dc9b0
Hans Hazairi <hanshazairi@gmail.com>
2021-07-26 00:09:49 +0800

    chore: 'ft_strtrim' improve code logic

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,34 +6,18 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 15:58:24 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/09 05:46:47 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/07/26 00:01:07 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	isset(char c, const char *set)
+static int	ft_isset(char c, const char *set)
 {
 	while (*set)
 		if (c == *set++)
 			return (1);
 	return (0);
-}
-
-static char	*ft_strndup(const char *s1, size_t n)
-{
-	char	*ret;
-	size_t	len;
-
-	if (n < ft_strlen(s1))
-		len = n;
-	else
-		len = ft_strlen(s1);
-	ret = malloc(len + 1);
-	if (!ret)
-		return (0);
-	ft_strlcpy(ret, s1, len + 1);
-	return (ret);
 }
 
 char	*ft_strtrim(const char *s1, const char *set)
@@ -46,10 +30,10 @@ char	*ft_strtrim(const char *s1, const char *set)
 		return (0);
 	start = (char *)s1;
 	end = start + ft_strlen(s1);
-	while (*start && isset(*start, set))
+	while (*start && ft_isset(*start, set))
 		++start;
-	while (start < end && isset(*(end - 1), set))
+	while (start < end && ft_isset(*(end - 1), set))
 		--end;
-	ret = ft_strndup(start, end - start);
+	ret = ft_substr(start, 0, end - start);
 	return (ret);
 }
